2be4250fa8c0e4083f0b6ee772a3fb4f129ea9aa
UjwalManjunath <muddu1990@gmail.com>
2013-04-16 11:57:36 -0400

    singleton class

---
+++ Clock.java
@@ -0,0 +1,26 @@
+public class Clock{
+    int time;  //time in cycles
+    static Clock clockPtr = null;
+    
+    //class is a singleton so constructor is private
+    private Clock(){
+        time = 0;
+    }
+    
+    //returns singleton instance
+    static Clock getInstance(){
+        if (clockPtr == null)
+            clockPtr = new Clock();
+        return clockPtr;
+    }
+    
+    //returns current time in cycles
+    int get(){
+        return time;
+    }
+    
+    //increment clock
+    void increment(){
+        time++;
+    }
+}
